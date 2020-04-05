b533bfffedea6aaad69fb58b6c2df7a12f27acda
Andrew Hao <andrewhao@gmail.com>
2020-04-05 15:59:34 -0700

    Use virtualenv python

--- label-wav.sh
+++ label-wav.sh
@@ -10,4 +10,4 @@ echo "Processing $INPUT_FILE..."
 sox "$INPUT_FILE" "$OUTPUT_FILE" trim 0 10 rate 22050
 
 echo "Classifying $OUTPUT_FILE..."
-python $(dirname $0)/app/label_wav_and_upload_to_server.py --graph=$(dirname $0)/graph.pb --labels=$(dirname $0)/conv_labels.txt --wav="$OUTPUT_FILE"
+./venv/bin/python $(dirname $0)/app/label_wav_and_upload_to_server.py --graph=$(dirname $0)/graph.pb --labels=$(dirname $0)/conv_labels.txt --wav="$OUTPUT_FILE"
