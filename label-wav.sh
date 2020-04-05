afd4d5822a21641f5935e1f3f13eac7649647ccd
Andrew Hao <andrewhao@gmail.com>
2020-04-05 15:57:09 -0700

    Truncate to length of 10 in label script

--- label-wav.sh
+++ label-wav.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+INPUT_FILE=$1
+OUTPUT_FILE="tmp/sample.wav"
 
-python $(dirname $0)/app/label_wav_and_upload_to_server.py --graph=$(dirname $0)/graph.pb --labels=$(dirname $0)/conv_labels.txt --wav="$1"
+mkdir -p tmp
+
+echo "Processing $INPUT_FILE..."
+sox "$INPUT_FILE" "$OUTPUT_FILE" trim 0 10 rate 22050
+
+echo "Classifying $OUTPUT_FILE..."
+python $(dirname $0)/app/label_wav_and_upload_to_server.py --graph=$(dirname $0)/graph.pb --labels=$(dirname $0)/conv_labels.txt --wav="$OUTPUT_FILE"
