0a2dd92f8a48b8ee3f95924accceffd0a03c8082
Andrew Hao <andrewhao@gmail.com>
2020-05-05 12:34:00 -0700

    Adding new labels and more steps to training

--- freeze.sh
+++ freeze.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-python app/freeze.py --start_checkpoint=./training/conv.ckpt-$CKPT_NUM --output_file=./graph.pb --clip_duration_ms=10000 --sample_rate=22050 --wanted_words=white_noise,room_empty,crying --data_dir=./data
+python app/freeze.py --start_checkpoint=./training/conv.ckpt-$CKPT_NUM --output_file=./graph.pb --clip_duration_ms=10000 --sample_rate=22050 --wanted_words=white_noise,room_empty,crying,talking,headbanging --data_dir=./data
 cp training/conv_labels.txt .
 
